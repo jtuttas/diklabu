e62a7dbce46049ea799505bed0d55bb5d80757b6
jtuttas <tuttas@mmbbs.de>
2015-10-21 12:25:05 +0200

    Fehler bei mehrere gleich Kurse

--- Diklabu/web/js/bscontroll.js
+++ Diklabu/web/js/bscontroll.js
@@ -75,40 +75,70 @@ function getCourseList() {
             $("#zweitWunschDropdown").empty();
             $("#drittWunschDropdown").empty();
             for (var i = 0; i < courseList.length; i++) {
-                $("#erstWunschDropdown").append('<li id="w1' + i + '" role="presentation" ><a kursid="' + i + '" role="menuitem" tabindex="-1" href="#f">' + courseList[i].TITEL + '</a></li>')
-                $("#zweitWunschDropdown").append('<li id="w2' + i + '" role="presentation" ><a kursid="' + i + '" role="menuitem" tabindex="-1" href="#f">' + courseList[i].TITEL + '</a></li>')
-                $("#drittWunschDropdown").append('<li id="w3' + i + '" role="presentation" ><a kursid="' + i + '"role="menuitem" tabindex="-1" href="#f">' + courseList[i].TITEL + '</a></li>')
+                $("#erstWunschDropdown").append('<li  id="w1' + i + '" role="presentation" ><a kursid="' + i + '" role="menuitem" tabindex="-1" href="#f">' + courseList[i].TITEL + '</a></li>')
+                $("#zweitWunschDropdown").append('<li  id="w2' + i + '" role="presentation" ><a kursid="' + i + '" role="menuitem" tabindex="-1" href="#f">' + courseList[i].TITEL + '</a></li>')
+                $("#drittWunschDropdown").append('<li  id="w3' + i + '" role="presentation" ><a kursid="' + i + '"role="menuitem" tabindex="-1" href="#f">' + courseList[i].TITEL + '</a></li>')
             }
             $('#erstWunschDropdown li > a').click(function (e) {
-                $('#erstWunsch').text(this.innerHTML);
-                var id = $(this).attr('kursid');
-                var course = courseList[id];
-                console.log("Gewählt wurde:" + course.TITEL);
-                wuensche[0] = course;
-                $("#w1" + id).addClass("disabled");
-                $("#w2" + id).addClass("disabled");
-                $("#w3" + id).addClass("disabled");
+                var index = $(this).attr('kursid');
+                console.log("index = "+index+" class="+$("#w1" + index).attr("class"));
+                if ($("#w1" + index).attr("class") != "disabled") {
+                    $('#erstWunsch').text(this.innerHTML);
+                    if (wuensche[0]!=undefined) {
+                        console.log("Es existierte bereits ein Erstwunsch mit id="+wuensche[0].id);
+                        var i=findWunschbyId(wuensche[0].id);
+                        console.log("Dieser hat den index "+i);
+                        $("#w1"+i).removeClass("disabled");
+                        $("#w2"+i).removeClass("disabled");
+                        $("#w3"+i).removeClass("disabled");
+                    }
+                    var course = courseList[index];
+                    console.log("Gewählt wurde:" + course.TITEL);
+                    wuensche[0] = course;
+                    $("#w1" + index).addClass("disabled");
+                    $("#w2" + index).addClass("disabled");
+                    $("#w3" + index).addClass("disabled");
+                }
             });
             $('#zweitWunschDropdown li > a').click(function (e) {
-                $('#zweitWunsch').text(this.innerHTML);
                 var id = $(this).attr('kursid');
-                var course = courseList[id];
-                console.log("Gewählt wurde:" + course.TITEL);
-                wuensche[1] = course;
-                $("#w1" + id).addClass("disabled");
-                $("#w2" + id).addClass("disabled");
-                $("#w3" + id).addClass("disabled");
+                if ($("#w2" + id).attr("class") != "disabled") {
+                    $('#zweitWunsch').text(this.innerHTML);
+                    if (wuensche[1]!=undefined) {
+                        console.log("Es existierte bereits ein Zweitwunsch mit id="+wuensche[1].id);
+                        var i=findWunschbyId(wuensche[1].id);
+                        console.log("Dieser hat den index "+i);
+                        $("#w1"+i).removeClass("disabled");
+                        $("#w2"+i).removeClass("disabled");
+                        $("#w3"+i).removeClass("disabled");
+                    }
+                    var course = courseList[id];
+                    console.log("Gewählt wurde:" + course.TITEL);
+                    wuensche[1] = course;
+                    $("#w1" + id).addClass("disabled");
+                    $("#w2" + id).addClass("disabled");
+                    $("#w3" + id).addClass("disabled");
+                }
             });
             $('#drittWunschDropdown li > a').click(function (e) {
-                $('#drittWunsch').text(this.innerHTML);
                 var id = $(this).attr('kursid');
-                var course = courseList[id];
-                console.log("Gewählt wurde:" + course.TITEL);
-                wuensche[2] = course;
-                $("#w1" + id).addClass("disabled");
-                $("#w2" + id).addClass("disabled");
-                $("#w3" + id).addClass("disabled");
-
+                if ($("#w3" + id).attr("class") != "disabled") {
+                    $('#drittWunsch').text(this.innerHTML);
+                    if (wuensche[2]!=undefined) {
+                        console.log("Es existierte bereits ein Drittwunsch mit id="+wuensche[2].id);
+                        var i=findWunschbyId(wuensche[2].id);
+                        console.log("Dieser hat den index "+i);
+                        $("#w1"+i).removeClass("disabled");
+                        $("#w2"+i).removeClass("disabled");
+                        $("#w3"+i).removeClass("disabled");
+                    }
+                    var course = courseList[id];
+                    console.log("Gewählt wurde:" + course.TITEL);
+                    wuensche[2] = course;
+                    $("#w1" + id).addClass("disabled");
+                    $("#w2" + id).addClass("disabled");
+                    $("#w3" + id).addClass("disabled");
+                }
             });
 
         }
@@ -117,6 +147,20 @@ function getCourseList() {
 
 
 }
+
+/**
+ * An welcher Stelle der coursliste findet sich welcher Kurs (ID)
+ * @param {type} id die Id des Kurses
+ * @returns {Number|j} die Position in der Kursliste
+ */
+function findWunschbyId( id) {
+    for ( j=0;j<courseList.length;j++) {
+        if (courseList[j].id==id) {
+            return j;
+        }        
+    }
+}
+
 toastr.options = {
     "closeButton": false,
     "debug": false,
