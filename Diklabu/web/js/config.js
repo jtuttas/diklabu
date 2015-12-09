73e7c6c0da651624b668cd2840d1b063a9c8911c
jtuttas <tuttas@mmbbs.de>
2015-12-09 16:28:33 +0100

    änderungen am surface

--- Diklabu/web/js/config.js
+++ Diklabu/web/js/config.js
@@ -2,7 +2,8 @@
  * TODO Konfiguration f. den Server
  */
 
-var SERVER = "http://172.31.0.26:8080";
+//var SERVER = "http://172.31.0.26:8080";
+var SERVER = "http://localhost:8080";
 //var SERVER = "http://service.joerg-tuttas.de:8081";
 
 var VERSION="V 1.22";
