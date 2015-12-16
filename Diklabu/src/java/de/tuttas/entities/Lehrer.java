38e7ea309ca2933cc2c3c5acf7b1c33f00ce7d58
jtuttas <tuttas@mmbbs.de>
2015-12-16 14:06:03 +0100

    Mit Lehrer Manager

---
+++ Diklabu/src/java/de/tuttas/entities/Lehrer.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.tuttas.entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+/**
+ *
+ * @author Jörg
+ */
+@Entity
+@NamedQueries({
+   @NamedQuery(name = "findAllTeachers", query= "select l from Lehrer l"),   
+})
+public class Lehrer implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(length=3)
+    private String id;
+    private String NNAME;
+    private String VNAME;
+
+    public void setNNAME(String NNAME) {
+        this.NNAME = NNAME;
+    }
+
+    
+    public String getNNAME() {
+        return NNAME;
+    }
+
+    public void setVNAME(String VNAME) {
+        this.VNAME = VNAME;
+    }
+
+    public String getVNAME() {
+        return VNAME;
+    }
+        
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Lehrer)) {
+            return false;
+        }
+        Lehrer other = (Lehrer) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "de.tuttas.entities.Lehrer[ id=" + id + " ]";
+    }
+    
+}
