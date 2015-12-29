b3e1a92c176285cf8fd55928d901dd72d2d06199
jtuttas <tuttas@mmbbs.de>
2015-12-29 14:31:10 +0100

    mit besserem Doku Servlet

--- Diklabu/src/java/de/tuttas/servlets/DokuServlet.java
+++ Diklabu/src/java/de/tuttas/servlets/DokuServlet.java
@@ -73,7 +73,9 @@ public class DokuServlet extends HttpServlet {
 
         String auth = request.getParameter("auth_token");
         String service = request.getParameter("service_key");
-        System.out.println("auth_token="+auth);
+        // TODO entfernen!
+        String debug = request.getParameter("debug");
+        System.out.println("auth_token=" + auth);
         if (request.getParameter("cmd") == null || request.getParameter("idklasse") == null || request.getParameter("from") == null) {
             System.out.println("Info zeigen");
             response.setContentType("text/html;charset=UTF-8");
@@ -86,15 +88,14 @@ public class DokuServlet extends HttpServlet {
                 out.println("</head>");
                 out.println("<body>");
                 out.println("<h1>Doku Servlet @ " + request.getContextPath() + "</h1>");
-                out.println("<a href='?cmd=verlauf&idklasse=3608&from=2015-09-08' target='_pdf'>Usage: ?cmd=verlauf&idklasse=3608&from=2015-09-08</a>");
+                out.println("<a href='?cmd=verlauf&idklasse=3608&from=2015-09-08&debug=true' target='_pdf'>Usage: ?cmd=verlauf&idklasse=3608&from=2015-09-08</a>");
                 out.println("</body>");
                 out.println("</html>");
             }
         } else {
-            if (service!=null && auth!=null && Authenticator.getInstance().isAuthTokenValid(service, auth)) {
+            if (debug != null || service != null && auth != null && Authenticator.getInstance().isAuthTokenValid(service, auth)) {
                 Klasse kl = em.find(Klasse.class, Integer.parseInt(request.getParameter("idklasse")));
                 System.out.println("Klasse=" + kl.getID_LEHRER());
-                response.addHeader("Content-Disposition", "attachment; filename=verlauf.pdf");
                 response.setContentType("application/pdf");
                 //Get the output stream for writing PDF object        
                 OutputStream out = response.getOutputStream();
@@ -121,74 +122,106 @@ public class DokuServlet extends HttpServlet {
                      System.out.println("Kann Template nicht finden");
                      }
                      */
-                    htmlString.append("<table border='1' align='center' width='100%'>");
-                    htmlString.append("<tr>");
-                    htmlString.append("<td rowspan=\"3\" width='150px'></td>");
-                    htmlString.append("<td align='center'><h2>Multi Media Berufsbildende Schulen Hannover</h2></td>");
-                    htmlString.append("<td colspan=\"2\" align='center'><b>Digitales Klassenbuch Unterrichtsverlauf</b></td>");
-                    htmlString.append("</tr>");
-                    htmlString.append("<tr>");
-                    htmlString.append("<td  align='center' rowspan=\"2\"><h3>Klasse/ Kurs: " + kl.getKNAME() + "</h3></td>");
-                    htmlString.append("<td  style=\"font-size: 11;\">Verantwortlicher: " + kl.getID_LEHRER() + "</td>");
-                    htmlString.append("<td  style=\"font-size: 11;\">geprüft</td>");
-                    htmlString.append("</tr>");
-                    htmlString.append("<tr>");
+                    String kopf = "";
+                    kopf += ("<table border='1' align='center' width='100%'>");
+                    kopf += ("<tr>");
+                    kopf += ("<td rowspan=\"3\" width='150px'></td>");
+                    kopf += ("<td align='center'><h2>Multi Media Berufsbildende Schulen Hannover</h2></td>");
+                    kopf += ("<td colspan=\"2\" align='center'><b>Digitales Klassenbuch Unterrichtsverlauf</b></td>");
+                    kopf += ("</tr>");
+                    kopf += ("<tr>");
+                    kopf += ("<td  align='center' rowspan=\"2\"><h3>Klasse/ Kurs: " + kl.getKNAME() + "</h3></td>");
+                    kopf += ("<td  style=\"font-size: 11;\">Verantwortlicher: " + kl.getID_LEHRER() + "</td>");
+                    kopf += ("<td  style=\"font-size: 11;\">geprüft</td>");
+                    kopf += ("</tr>");
+                    kopf += ("<tr>");
                     DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
                     Calendar c = df.getCalendar();
                     c.setTimeInMillis(System.currentTimeMillis());
                     String dat = c.get(Calendar.DAY_OF_MONTH) + "." + (c.get(Calendar.MONTH) + 1) + "." + c.get(Calendar.YEAR);
-                    htmlString.append("<td  style=\"font-size: 11;\">Ausdruck am: " + dat + "</td>");
-                    htmlString.append("<td  style=\"font-size: 11;\">Datum</td>");
-                    htmlString.append("</tr>");
-                    htmlString.append("</table>");
-                    htmlString.append("<p>&nbsp;</p>");
-
+                    kopf += ("<td  style=\"font-size: 11;\">Ausdruck am: " + dat + "</td>");
+                    kopf += ("<td  style=\"font-size: 11;\">Datum</td>");
+                    kopf += ("</tr>");
+                    kopf += ("</table>");
+                    kopf += ("<p>&nbsp;</p>");
+                    htmlString.append(kopf);
                     /* Verlauf einfügen */
                     Query query = em.createNamedQuery("findVerlaufbyKlasse");
                     query.setParameter("paramKName", kl.getKNAME());
                     DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                    Date parsed = (Date) dateFormat.parse(request.getParameter("from"));
-                    query.setParameter("paramFromDate", new java.sql.Date(parsed.getTime()));
-                    System.out.println("setze From auf " + new java.sql.Date(parsed.getTime()));
+                    Date parsedFrom = (Date) dateFormat.parse(request.getParameter("from"));
+                    query.setParameter("paramFromDate", new java.sql.Date(parsedFrom.getTime()));
+                    System.out.println("setze From auf " + new java.sql.Date(parsedFrom.getTime()));
+                    Date parsedTo;
                     if (request.getParameter("to") == null) {
-
                         query.setParameter("paramToDate", new java.sql.Date(System.currentTimeMillis()));
+                        parsedTo = new java.sql.Date(System.currentTimeMillis());
                     } else {
                         dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                        parsed = (Date) dateFormat.parse(request.getParameter("to"));
-                        query.setParameter("paramToDate", new java.sql.Date(parsed.getTime()));
+                        parsedTo = (Date) dateFormat.parse(request.getParameter("to"));
+                        query.setParameter("paramToDate", new java.sql.Date(parsedTo.getTime()));
                     }
+                    response.addHeader("Content-Disposition", "attachment; filename=verlauf_"+kl.getKNAME()+"_"+new java.sql.Date(parsedFrom.getTime()).toString()+"-"+parsedTo.toString()+".pdf");
 
                     List<Verlauf> verlauf = query.getResultList();
                     System.out.println("Result List:" + verlauf);
                     htmlString.append("<table  align='center' width='100%' style=\"border: 2px solid black; border-collapse: collapse;\">");
-                    htmlString.append("<tr >");
-                    htmlString.append("<td  width='12%' style=\"font-size: 11;border: 1px solid black; \"><b>Datum</b></td>");
-                    htmlString.append("<td  width='3%' style=\"font-size: 11;border: 1px solid black;\"><b>LK</b></td>");
-                    htmlString.append("<td  width='5%' style=\"font-size: 11;border: 1px solid black;\"><b>LF</b></td>");
-                    htmlString.append("<td  width='7%' style=\"font-size: 11;border: 1px solid black;\"><b>Stunde</b></td>");
-                    htmlString.append("<td  style=\"font-size: 11;border: 1px solid black;\"><b>Inhalt</b></td>");
-                    htmlString.append("<td  style=\"font-size: 11;border: 1px solid black;\"><b>Bemerkungen</b></td>");
-                    htmlString.append("<td  style=\"font-size: 11;border: 1px solid black;\"><b>Lernsituation</b></td>");
 
-                    htmlString.append("</tr>");
+                    String tagZeile = "";
+                    tagZeile += ("<tr >");
+                    tagZeile += ("<td  width='3%' style=\"font-size: 11;border: 1px solid black;\"><b>LK</b></td>");
+                    tagZeile += ("<td  width='5%' style=\"font-size: 11;border: 1px solid black;\"><b>LF</b></td>");
+                    tagZeile += ("<td  width='7%' style=\"font-size: 11;border: 1px solid black;\"><b>Stunde</b></td>");
+                    tagZeile += ("<td  style=\"font-size: 11;border: 1px solid black;\"><b>Inhalt</b></td>");
+                    tagZeile += ("<td  style=\"font-size: 11;border: 1px solid black;\"><b>Bemerkungen</b></td>");
+                    tagZeile += ("<td  style=\"font-size: 11;border: 1px solid black;\"><b>Lernsituation</b></td>");
+                    tagZeile += ("</tr>");
+                    htmlString.append(tagZeile);
+                    
+                    String tag = " ";
+                    document.open();
                     for (Verlauf v : verlauf) {
-                        htmlString.append("<tr>");
                         String str = v.getDATUM().toString();
-                        htmlString.append("<td width='12%' style=\"font-size: 11;border: 1px solid black;\">" + str.substring(0, str.indexOf(" ")) + "</td>");
-                        htmlString.append("<td width='3%' style=\"font-size: 11;border: 1px solid black;\">" + v.getID_LEHRER() + "</td>");
-                        htmlString.append("<td width='5%' style=\"font-size: 11;border: 1px solid black;\">" + v.getID_LERNFELD() + "</td>");
-                        htmlString.append("<td width='7%' style=\"font-size: 11;border: 1px solid black;\">" + v.getSTUNDE() + "</td>");
-                        htmlString.append("<td style=\"font-size: 11;border: 1px solid black;\">" + v.getINHALT() + "</td>");
-                        htmlString.append("<td style=\"font-size: 11;border: 1px solid black;\">" + v.getBEMERKUNG() + "</td>");
-                        htmlString.append("<td style=\"font-size: 11;border: 1px solid black;\">" + v.getAUFGABE() + "</td>");
+                        if (str.compareTo(tag) == 0) {
+                            htmlString.append("<tr>");
+                            htmlString.append("<td width='3%' style=\"font-size: 11;border: 1px solid black;\">" + v.getID_LEHRER() + "</td>");
+                            htmlString.append("<td width='5%' style=\"font-size: 11;border: 1px solid black;\">" + v.getID_LERNFELD() + "</td>");
+                            htmlString.append("<td width='7%' style=\"font-size: 11;border: 1px solid black;\">" + v.getSTUNDE() + "</td>");
+                            htmlString.append("<td style=\"font-size: 11;border: 1px solid black;\">" + v.getINHALT() + "</td>");
+                            htmlString.append("<td style=\"font-size: 11;border: 1px solid black;\">" + v.getBEMERKUNG() + "</td>");
+                            htmlString.append("<td style=\"font-size: 11;border: 1px solid black;\">" + v.getAUFGABE() + "</td>");
+                            htmlString.append("</tr>");
+                        } // ein neuer Tag
+                        else {
+                            // Jede Woche eine neue Seite!
+                            if (v.getWochentag().compareTo("Mo.") == 0) {
+                                htmlString.append("</table>");
+                                System.out.println("html String=" + htmlString.toString());
+                                //document.add(new Paragraph("Tutorial to Generate PDF using Servlet"));
+                                InputStream is = new ByteArrayInputStream(htmlString.toString().getBytes());
+                                // Bild einfügen
+                                String url = "http://www.mmbbs.de/fileadmin/template/mmbbs/gfx/mmbbs_logo_druck.gif";
+                                Image image = Image.getInstance(url);
+                                image.setAbsolutePosition(45f, 720f);
+                                image.scalePercent(50f);
+                                document.add(image);
+                                XMLWorkerHelper.getInstance().parseXHtml(writer, document, is);
+                                document.newPage();
+                                htmlString = new StringBuilder();
+                                htmlString.append(kopf);
+                                htmlString.append("<table  align='center' width='100%' style=\"border: 2px solid black; border-collapse: collapse;\">");
+                                htmlString.append(tagZeile);
+                                System.out.println("weiter mit neuer Seite");
+                            }
+                            htmlString.append("<tr>");
+                            htmlString.append("<td colspan=\"6\" align=\"center\" style=\"background-color: #cccccc; padding:4px;border: 1px solid black;\">" + v.getWochentag() + " " + str.substring(0, str.indexOf(" ")) + "</td>");
+                            htmlString.append("</tr>");
 
-                        htmlString.append("</tr>");
-
+                            tag = str;
+                        }
                     }
                     htmlString.append("</table>");
-
-                    document.open();
+                    System.out.println("html String Rest=" + htmlString.toString());
                     //document.add(new Paragraph("Tutorial to Generate PDF using Servlet"));
                     InputStream is = new ByteArrayInputStream(htmlString.toString().getBytes());
                     // Bild einfügen
@@ -197,7 +230,6 @@ public class DokuServlet extends HttpServlet {
                     image.setAbsolutePosition(45f, 720f);
                     image.scalePercent(50f);
                     document.add(image);
-
                     XMLWorkerHelper.getInstance().parseXHtml(writer, document, is);
                     document.close();
 
