87e2e4c0ef1c185b1f460c85491dc81d97b88dcd
jtuttas <tuttas@mmbbs.de>
2016-06-29 13:10:38 +0200

    Kurswahl mobile geht auch wieder

--- Diklabu/web/js/controller.js
+++ Diklabu/web/js/controller.js
@@ -3,108 +3,104 @@
 var courseList;
 var wunsch;
 var wuensche = new Array(3);
-var credentials;
+var courseList;
 $(document).ready(function () {
-    
+    $("#btnzurueck").click(function () {
+        console.log("btnzurueck Click");
+        performLogout(sessionStorage.benutzer,sessionStorage.kennwort, function() {
+            sessionStorage.clear();
+            $("#benutzername").val("");
+            $("#kennwort").val("");
+            $.mobile.changePage('#login');
+        });
+
+    });
+
     $("#btnLogin").click(function () {
-        if ($("#name").val()=="" ||$("#vorname").val()=="" || $("#gebdatum").val()=="") {
-           toast("Bitte füllen Sie das Formular komplett aus!");
-       } 
-       else {
-        var pupil = {
-            name: $("#name").val(),
-            vorName: $("#vorname").val(),
-            gebDatum: $("#gebdatum").val()
-        };
-        console.log(JSON.stringify(pupil));
-        $.ajax({
-            url: SERVER + "/Diklabu/api/v1/kurswahl/login",
-            type: "POST",
-            data: JSON.stringify(pupil),
-            contentType: "application/json; charset=UTF-8",
-            dataType: "json",
-            success: function (data) {
-                console.log("receive" + JSON.stringify(data));
-                if (data.login) {
-                    credentials=data;
-                    console.log("bisherige wünsche :"+credentials.courses);
-                    if (credentials.courses!=undefined && credentials.courses.length!=0) {
+        if ($("#benutzername").val() == "" || $("#kennwort").val() == "") {
+            toast("Bitte füllen Sie das Formular komplett aus!");
+        }
+        else {
+            performLogin($("#benutzername").val(), $("#kennwort").val(), function (data) {
+                sessionStorage.myself = data.ID;
+                sessionStorage.benutzer=$("#benutzername").val();
+                sessionStorage.kennwort=$("#kennwort").val();
+                
+                getKurswunsch(function (data) {
+                    if (data.courseList != undefined) {
+                        courseList=data;
                         $.mobile.changePage('#results');
                     }
-                    else getCourseList();
-                    
-                }
-                else {
-                    toast(data.msg);
-                }
-            }
-        });
-    }
+                    else {
+                        getCourseList();
+                    }
+                });
+            }, function () {
+                toast("Anmeldedaten ungültig");
+            });
+        }
     });
 
     $("#btnErstwunsch").click(function () {
-        wunsch=0;
+        wunsch = 0;
         $.mobile.changePage('#wpks');
     });
     $("#btnZweitwunsch").click(function () {
-        wunsch=1;
+        wunsch = 1;
         $.mobile.changePage('#wpks');
     });
     $("#btnDrittwunsch").click(function () {
-        wunsch=2;
+        wunsch = 2;
         $.mobile.changePage('#wpks');
     });
-    
+
     $("#btnCommit").click(function () {
-       if (wuensche[0]==undefined || wuensche[1]==undefined || wuensche[2]==undefined) {
-           toast("Bitte drei Kurse wählen!");
-       } 
-       else {
-           console.log("Wunsche=" + JSON.stringify(wuensche));
-           var ticketing = {
-               credential: credentials,
-               courseList: wuensche               
-           };
+        if (wuensche[0] == undefined || wuensche[1] == undefined || wuensche[2] == undefined) {
+            toast("Bitte drei Kurse wählen!");
+        }
+        else {
+            console.log("Wunsche ==" + JSON.stringify(wuensche));
+            var ticketing = {
+                courseList: wuensche
+            };
             console.log("ticketing=" + JSON.stringify(ticketing));
             $.ajax({
-            url: SERVER + "/Diklabu/api/v1/kurswahl/buchen",
-            type: "POST",
-            contentType: "application/json; charset=UTF-8",
-            dataType: "json",
-            data: JSON.stringify(ticketing),
-            success: function (data) {
-                console.log("buchen finished" + JSON.stringify(data));
-                toast(data.msg);
-                $("#name").val("");
-                $("#vorname").val("");
-                $("#gebdatum").val("");
-                $.mobile.changePage('#login');
-                credentials="";
-            }
-        });
-           
-       }
+                url: SERVER + "/Diklabu/api/v1/sauth/kursbuchung/"+sessionStorage.myself,
+                type: "POST",
+                contentType: "application/json; charset=UTF-8",
+                dataType: "json",
+                data: JSON.stringify(ticketing),
+                success: function (data) {
+                    console.log("buchen finished" + JSON.stringify(data));
+                    toast(data.msg);
+                    if (data.success) {
+                        $("#benutzername").val("");
+                        $("#kennwort").val("");
+                        $.mobile.changePage('#login');
+                        performLogout(sessionStorage.benutzer,sessionStorage.kennwort);
+                    }
+                }
+            });
+
+        }
     });
-    
-    $("#btnzuruck").click( function () {
-         credentials=undefined;
- 
-    });
-    
+
+   
+
     $("#btnWaehlen").click(function () {
         console.log("btnWaehnlen click");
-        var count=0;
+        var count = 0;
         $("input[name*=slot]:checked").each(function () {
-            count=1;
+            count = 1;
             var id = $(this).attr('id');
-            console.log("Gewählt wurde ID="+id);
+            console.log("Gewählt wurde ID=" + id);
             var course = courseList[id];
-            console.log("Gewählt wurde:"+course.TITEL);
-            wuensche[wunsch]=course;
-            console.log("Wuensche="+JSON.stringify(wuensche));
-            
+            console.log("Gewählt wurde:" + course.TITEL);
+            wuensche[wunsch] = course;
+            console.log("Wuensche=" + JSON.stringify(wuensche));
+
         });
-        if (count==0) {
+        if (count == 0) {
             toast("Bitte einen Kurs wählen!");
         }
         else {
@@ -112,74 +108,79 @@ $(document).ready(function () {
         }
     });
 
-$("#about").on("pagebeforeshow", function (e) {
-    $("#version").text(VERSION); 
-});
-   
-     $("#results").on("pagebeforeshow", function (event) {
-         console.log("Anzeige der Wünsche:");
-         $("#erstwunschresult").text("1. Wunsch: "+credentials.courses[0].TITEL+" ("+credentials.courses[0].ID_LEHRER+")");
-         $("#zweitwunschresult").text("2. Wunsch: "+credentials.courses[1].TITEL+" ("+credentials.courses[1].ID_LEHRER+")");
-         $("#drittwunschresult").text("3. Wunsch: "+credentials.courses[2].TITEL+" ("+credentials.courses[2].ID_LEHRER+")");
-         
-         if (credentials.selectedCourse!=undefined) {
-             $("#zugeteilt").text("Ihnen wurde der Kurs '"+credentials.selectedCourse.TITEL+"' zugewiesen!");
-         }
-         else {
-             $("#zugeteilt").text("Ihnen wurde noch kein Kurs zugeteilt!");
-         }
-     });
-     
+    $("#about").on("pagebeforeshow", function (e) {
+        $("#version").text(VERSION);
+    });
+
+    $("#results").on("pagebeforeshow", function (event) {
+        console.log("Anzeige der Wünsche:");
+        $("#erstwunschresult").text("1. Wunsch: " + courseList.courseList[0].TITEL + " (" + courseList.courseList[0].ID_LEHRER + ")");
+        $("#zweitwunschresult").text("2. Wunsch: " + courseList.courseList[1].TITEL + " (" + courseList.courseList[1].ID_LEHRER + ")");
+        $("#drittwunschresult").text("3. Wunsch: " + courseList.courseList[2].TITEL + " (" + courseList.courseList[2].ID_LEHRER + ")");
+
+        if (courseList.selectedCourse != undefined) {
+            $("#zugeteilt").text("Ihnen wurde der Kurs '" + courseList.selectedCourse.TITEL + "' zugewiesen!");
+        }
+        else {
+            $("#zugeteilt").text("Ihnen wurde noch kein Kurs zugeteilt!");
+        }
+        
+        
+    });
+
     $("#wuensche").on("pagebeforeshow", function (event) {
-        if (courseList==undefined) $.mobile.changePage('#login');
-        else {console.log("wuensche show");
-        if (wuensche[0]!=null) {
-            $("#erstwunsch").text(wuensche[0].TITEL+ " ("+wuensche[0].ID_LEHRER+")");
-        }
+        if (courseList == undefined)
+            $.mobile.changePage('#login');
         else {
-            $("#erstwunsch").text("kein Kurs gewählt");
+            console.log("wuensche show");
+            if (wuensche[0] != null) {
+                $("#erstwunsch").text(wuensche[0].TITEL + " (" + wuensche[0].ID_LEHRER + ")");
+            }
+            else {
+                $("#erstwunsch").text("kein Kurs gewählt");
+            }
+            if (wuensche[1] != null) {
+                $("#zweitwunsch").text(wuensche[1].TITEL + " (" + wuensche[1].ID_LEHRER + ")");
+            }
+            else {
+                $("#zweitwunsch").text("kein Kurs gewählt");
+            }
+            if (wuensche[2] != null) {
+                $("#drittwunsch").text(wuensche[2].TITEL + " (" + wuensche[2].ID_LEHRER + ")");
+            }
+            else {
+                $("#drittwunsch").text("kein Kurs gewählt");
+            }
         }
-        if (wuensche[1]!=null) {
-            $("#zweitwunsch").text(wuensche[1].TITEL+ " ("+wuensche[1].ID_LEHRER+")");
-        }
-        else {
-            $("#zweitwunsch").text("kein Kurs gewählt");
-        }
-        if (wuensche[2]!=null) {
-            $("#drittwunsch").text(wuensche[2].TITEL+ " ("+wuensche[2].ID_LEHRER+")");
-        }
-        else {
-            $("#drittwunsch").text("kein Kurs gewählt");
-        }
-    }
     });
     $("#wpks").on("pagebeforeshow", function (event) {
-        if (courseList==undefined) $.mobile.changePage('#login');
+        if (courseList == undefined)
+            $.mobile.changePage('#login');
         else {
-        console.log("wpks show");
-        $("#wpklist").empty();
-        $('#wpklist').append('<fieldset data-role="controlgroup" id="cgrp">');
-        console.log("Wuensche sind:"+JSON.stringify(wuensche));
-        for (i = 0; i < courseList.length; i++) {
-            $('#wpklist').append('<input type="radio" name="slot" id="' + i + '" value="' + courseList[i].TITEL +'" /><label for="' + i + '">' + courseList[i].TITEL + '  ('+courseList[i].ID_LEHRER+')</label>');
-            for (j=0;j<=2;j++) {
-                if (wuensche[j]!=undefined) {
-                    if (courseList[i].id==wuensche[j].id) {
-                        $('#' + i).attr('disabled', true);
+            console.log("wpks show");
+            $("#wpklist").empty();
+            $('#wpklist').append('<fieldset data-role="controlgroup" id="cgrp">');
+            console.log("Wuensche sind:" + JSON.stringify(wuensche));
+            for (i = 0; i < courseList.length; i++) {
+                $('#wpklist').append('<input type="radio" name="slot" id="' + i + '" value="' + courseList[i].TITEL + '" /><label for="' + i + '">' + courseList[i].TITEL + '  (' + courseList[i].ID_LEHRER + ')</label>');
+                for (j = 0; j <= 2; j++) {
+                    if (wuensche[j] != undefined) {
+                        if (courseList[i].id == wuensche[j].id) {
+                            $('#' + i).attr('disabled', true);
+                        }
+
                     }
-                    
                 }
-            }            
-        }
-        $('#wpklist').append('</fieldset>');
-        $("#wpklist").trigger('create');
+            }
+            $('#wpklist').append('</fieldset>');
+            $("#wpklist").trigger('create');
         }
     });
 });
 
 function getCourseList() {
     $.ajax({
-        url: SERVER + "/Diklabu/api/v1/kurswahl/getcourses",
+        url: SERVER + "/Diklabu/api/v1/noauth/getcourses",
         type: "GET",
         contentType: "application/json; charset=UTF-8",
         dataType: "json",
@@ -190,9 +191,6 @@ function getCourseList() {
             $.mobile.changePage('#wuensche');
         }
     });
-
-
-
 }
 
 var toast = function (msg) {
@@ -210,5 +208,114 @@ var toast = function (msg) {
                 $(this).remove();
             });
 };
+
+function performLogout(benutzer, kennwort, callback) {
+     console.log("perform Logout benutzer=" + benutzer + " kennwort=" + kennwort);
+    var myData = {
+        "benutzer": benutzer,
+        "kennwort": kennwort
+    };
+
+    $.ajax({
+        cache: false,
+        contentType: "application/json; charset=UTF-8",
+        headers: {
+            "auth_token": sessionStorage.auth_token
+        },
+        dataType: "json",
+        url: "/Diklabu/api/v1/auth/logout/",
+        type: "POST",
+        data: JSON.stringify(myData),
+        success: function (jsonObj, textStatus, xhr) {
+            sessionStorage.clear();
+            console.log("--> Logout: " + JSON.stringify(jsonObj));
+            if (callback != undefined) {
+                callback(jsonObj);
+            }
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            
+            console.log("HTTP Status: " + xhr.status);
+            console.log("Error textStatus: " + textStatus);
+            console.log("Error thrown: " + errorThrown);
+        }
+    });
+}
+
+function performLogin(benutzer, kennwort, callback, error) {
+    console.log("perform Login benutzer=" + benutzer + " kennwort=" + kennwort);
+
+    var myData = {
+        "benutzer": benutzer,
+        "kennwort": kennwort
+    };
+    $.ajax({
+        cache: false,
+        contentType: "application/json; charset=UTF-8",
+        dataType: "json",
+        url: "/Diklabu/api/v1/auth/login/",
+        type: "POST",
+        data: JSON.stringify(myData),
+        success: function (jsonObj, textStatus, xhr) {
+            sessionStorage.auth_token=jsonObj.auth_token;
+            callback(jsonObj);
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            
+            console.log("HTTP Status: " + xhr.status);
+            console.log("Error textStatus: " + textStatus);
+            console.log("Error thrown: " + errorThrown);
+            if (error != undefined) {
+                console.log("call error Handler!");
+                error();
+            }
+        }
+    });
+}
+
+function submitKurswunsch(callback) {
+    console.log("Wunsche=" + JSON.stringify(wuensche));
+
+    var ticketing = {
+        courseList: wuensche
+    };
+    console.log("ticketing=" + JSON.stringify(ticketing));
+    $.ajax({
+        url: SERVER + "/Diklabu/api/v1/sauth/kursbuchung/" + sessionStorage.myself,
+        type: "POST",
+        contentType: "application/json; charset=UTF-8",
+        dataType: "json",
+        data: JSON.stringify(ticketing),
+        success: function (data) {
+            callback(data);
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            toastr["error"]("Kursbuchung fehlgeschlagen", "Fehler!");
+            console.log("HTTP Status: " + xhr.status);
+            console.log("Error textStatus: " + textStatus);
+            console.log("Error thrown: " + errorThrown);
+        }
+    });
+
+}
+
+function getKurswunsch(callback) {
+    $.ajax({
+        url: SERVER + "/Diklabu/api/v1/sauth/kursbuchung/" + sessionStorage.myself,
+        type: "GET",
+        contentType: "application/json; charset=UTF-8",
+        dataType: "json",
+        success: function (data) {
+            callback(data);
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            toastr["error"]("Kursbuchungsabfrage fehlgeschlagen", "Fehler!");
+            console.log("HTTP Status: " + xhr.status);
+            console.log("Error textStatus: " + textStatus);
+            console.log("Error thrown: " + errorThrown);
+        }
+    });
+}
+
 
 
