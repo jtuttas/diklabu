0caba2b7359d79538d536684f3cb7f41a4b30952
jtuttas <tuttas@mmbbs.de>
2016-07-13 15:27:22 +0200

    mit send-mail.ps1

---
+++ Diklabu/PowerShell/send-Mail.ps1
@@ -0,0 +1,5 @@
+﻿# Email Senden via Powershell
+$config=Get-Content "$PSScriptRoot/config.json" | ConvertFrom-json
+$password = $config.pass | ConvertTo-SecureString -asPlainText -Force
+$credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $config.user, $password
+Send-MailMessage -Body "Ein test" -From "tuttas@mmbbs.de" -To "jtuttas@gmx.net" -SmtpServer $config.smtphost -Credential $credentials -Subject "via Powershell" -UseSsl -Port $config.port -Attachments "$PSScriptRoot/antwort.csv"
