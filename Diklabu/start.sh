0c6e1a00f759d6d1fa649d40d1ae0f522be4bb33
jtuttas <tuttas@mmbbs.de>
2016-11-02 16:57:37 +0100

    nes sql build script

--- Diklabu/start.sh
+++ Diklabu/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Starting Firebird DB Server"
 /usr/sbin/fbguard &
+echo "Building Database structure"
+isql-fb -i /home/diklabu/diklabu/Diklabu/Sql_buils.sql        
 echo "Starting Glassfish Application Server"
 /glassfish4/bin/asadmin start-domain
 echo "Starting SSH Server"
