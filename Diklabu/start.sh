22060166bd858cce928379050618a7e67c0d0798
jtuttas <tuttas@mmbbs.de>
2016-11-03 14:44:03 +0100

    neues start Script

--- Diklabu/start.sh
+++ Diklabu/start.sh
@@ -3,5 +3,7 @@ echo "Starting Firebird DB Server"
 /usr/sbin/fbguard &
 echo "Starting Glassfish Application Server"
 /glassfish4/bin/asadmin start-domain
+echo "Deploying diklabu Application"
+/glassfish4/bin/asadmin --user admin --passwordfile=/tmp/glassfishpwd deploy /home/diklabu/diklabu/Diklabu/dist/Diklabu.war 
 echo "Starting SSH Server"
 /usr/sbin/sshd -D &
