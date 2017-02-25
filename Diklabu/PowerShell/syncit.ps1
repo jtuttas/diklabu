929338fe06489b3d2b2ec8ec7ae7bc04330a9297
jtuttas <tuttas@mmbbs.de>
2017-02-25 14:54:01 +0100

    PS Anpassungen

--- Diklabu/PowerShell/syncit.ps1
+++ Diklabu/PowerShell/syncit.ps1
@@ -17,7 +17,7 @@ try {
     #$body+="Es wurden "+$r1.delete+" Lehrer geloescht!`r`n";
     #$body+="Es traten "+$r1.error+" Fehler auf (siehe Protokoll im Anhang)`r`n";
     #$body+="`r`n";
-    $body+="set-emails.ps1`r`n";
+    $body+="`r`n`r`nset-emails.ps1`r`n";
     $r2=set-emails -force -Verbose
     $body+="Es wurden "+$r2.total+" Schueler bearbeitet`r`n";
     $body+="Es wurden "+$r2.update+" Schuelermails aktualisiert`r`n";
@@ -30,8 +30,11 @@ catch {
 }
 try {
     ## Moodle gloable gruppe Sync
+    $body+="`r`n`r`nSynchronisiere Moodle Cohorts with Seafile CSV"
     Login-Moodle
+    $body+="`r`nLogin Moodle OK"
     Sync-MoodleTeams -url https://seafile.mm-bbs.de/f/a743d75f83/?raw=1 -Verbose
+    $body+="`r`nSynchronisation erfolgt"
 }
 catch {
     Write-Error $_.Exception.Message
@@ -44,5 +47,10 @@ if (-not $Global:logins["smtp"]) {
 }
 else {
     #send-mailreport -from tuttas@mmbbs.de -to jtuttas@gmx.net -subject "Synchronisationsscript durchgelaufen" -body $body -attachment "$PSScriptRoot/../../../out_lehrer.txt","$PSScriptRoot/../../../out_schueler.txt"
-    send-mailreport -from tuttas@mmbbs.de -to jtuttas@gmx.net -subject "Synchronisationsscript durchgelaufen" -body $body -attachment "$PSScriptRoot/../../../out_schueler.txt"
+    if (Test-Path "$PSScriptRoot/../../../out_schueler.txt") {
+        send-mailreport -from tuttas@mmbbs.de -to jtuttas@gmx.net -subject "Synchronisationsscript durchgelaufen" -body $body -attachment "$PSScriptRoot/../../../out_schueler.txt"
+    }
+    else {
+        send-mailreport -from tuttas@mmbbs.de -to jtuttas@gmx.net -subject "Synchronisationsscript durchgelaufen" -body $body -attachment "$PSScriptRoot/../../../out_schueler.txt"
+    }
 }
