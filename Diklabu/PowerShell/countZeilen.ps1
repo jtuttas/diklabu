8b5b914e9517b4288d0a39ad16b0cc57b7289d70
jtuttas <tuttas@mmbbs.de>
2017-05-10 11:03:26 +0200

    Mit Count Zeilen

---
+++ Diklabu/PowerShell/countZeilen.ps1
@@ -0,0 +1,11 @@
+﻿function countZeilen($pfad) {
+Get-ChildItem -Recurse -Path $pfad | Where-Object {$_.Extension -eq ".java" -or $_.Extension -eq ".js" -or $_.Extension -eq ".html" -or $_.Extension -eq ".ps1"} |
+    ForEach-Object {
+        $out="" | Select-Object -Property Verzeichnis,Name,Zeilen
+        $out.Verzeichnis = $_.DirectoryName
+        $out.Name=$_.Name
+        $content = Get-Content $_.FullName
+        $out.Zeilen=$content.Length
+        $out
+    }
+}
