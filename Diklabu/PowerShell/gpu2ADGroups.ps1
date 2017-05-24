2bda6e55a96a03c121a81ea3768b3366ef8ec0e7
Jörg <Jrg@Z240>
2017-05-24 08:05:19 +0200

    Mit Conatins

--- Diklabu/PowerShell/gpu2ADGroups.ps1
+++ Diklabu/PowerShell/gpu2ADGroups.ps1
@@ -22,7 +22,7 @@
 
 #>
 
-function get-classTeachersTeam {
+function Import-Untis {
     [CmdletBinding()]
     Param
     (    
@@ -39,7 +39,6 @@ function get-classTeachersTeam {
        $csvhead = "Unr;std1;std2;std3;klasse;lol;fach;raum;uebertragen;feld_j;wstd;halbj;statkuerzel;feld_n;datumstart;datumend;feld_q;SIst;LIst;raum2;uform;feld_v;feld_w;feld_x;feld_y;feld_z;feld_aa;feld_ab;feld_ac;feld_ad;feld_ae;feld_af;feld_ag;feld_ah;feld_ai;feld_aj;feld_ak;feld_al;feld_am;feld_an;feld_ao"
        $out_delimiter="," #Trennzeichen für die Lehrerliste einer Klasse
     
-       try{
        if ((get-content $path -totalcount 1) -ne $csvhead) {
           #Kopfzeile einfügen falls noch nicht vorhanden
           $ftext = get-content $path -Encoding UTF8 #alten content merken  
@@ -50,42 +49,21 @@ function get-classTeachersTeam {
           # msg "Kopfzeile in GPU Exportdatei eingefügt" $s
       }
       
-      # Untis csv einlesen
-      $gpu_unterrichte=Import-Csv -Delimiter ";" -Path $path -Encoding UTF8
-      
-      $out=@{}
-      foreach ($gpu_unterricht in $gpu_unterrichte){
-        #Wenn .fach bestimmte Bezeichnungen enthält, wird LoL nicht die Liste eingepflegt
-        $eintragen = $true
-        foreach($keinLFoderFach in $blacklist){
-          if(($gpu_unterricht.fach).contains($keinLFoderFach)){$eintragen=$false}
-        }
-
-        if($eintragen){
-            #falls Eintrag für diese Klasse noch leer, wird das jetzt einfach eingetragen
-            if ((!$out["Klassenteam-$($gpu_unterricht.klasse)"]) -or ($out["Klassenteam-$($gpu_unterricht.klasse)"] -eq "")){
-                $r=@();
-                $r+=$gpu_unterricht.lol
-                $out["Klassenteam-$($gpu_unterricht.klasse)"]=$r
-            }
-            elseif (!$out["Klassenteam-$($gpu_unterricht.klasse)"].contains($gpu_unterricht.lol)) {
-             #LoL noch nicht eingetragen, hinten anhängen
-             $r=$out["Klassenteam-$($gpu_unterricht.klasse)"]
-             $r+=$gpu_unterricht.lol
-             $out["Klassenteam-$($gpu_unterricht.klasse)"]=$r
             
-            }
+      $out=@{}
+      import-csv C:\Temp\GPU002-kurz.txt -Delimiter ";" | Where-Object {-not ($blacklist -contains $_.fach)} | ForEach-Object {
+        if ($out[$_.klasse] -eq $null) {
+            $data=@();
+            $data+=$_.lol;
+            $out[$_.klasse]=$data
         }
-        
-      }#ende foreach
-       $out
+        elseif (-not ($out[$_.klasse] -contains $_.lol)) {
+            $data+=$_.lol;
+            $out[$_.klasse]=$data
+        }
+      }
+      $out
       
-
-     }#ende try
-     
-     catch {
-            Write-Error "Get-classTeachersTeam: Status-Code"$_.Exception.Response.StatusCode.value__ " "$_.Exception.Response.StatusDescription 
-     }
     }
 
 }#ende get-classTeachersTeam function
