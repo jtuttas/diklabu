ed456729a3db93be124c4bbe0722b1218a67ccc2
jtuttas <jtuttas@gmx.net>
2019-01-13 15:09:51 +0100

    mit test

---
+++ Diklabu/PowerShell/test.ps1
@@ -0,0 +1,12 @@
+﻿Get-ChildItem 'C:\Users\jtutt\Documents\NetBeansProjects\diklabu\Diklabu\PowerShell' | ForEach-Object {
+    if ($_.Directory -and $_.Name.Contains("diklabu")) {
+        Write-Host "Teste $($_.Name)" -BackgroundColor DarkGreen    
+        try {
+            & $_.FullName
+        }
+        catch {
+            Write-Error "Fehler in Zeile $($_.Exception)"
+        } 
+    }
+  
+}
